925c500e8cd4416f57b17c958ce4cb1d617e61d8
naduni99x <102135074+naduni99x@users.noreply.github.com>
2023-01-13 22:31:10 +0530

    #3 setting material UI and creating modules

---
+++ apps/client/src/app/core/components/auth/auth.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'slicetime-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
+})
+export class AuthComponent {}
