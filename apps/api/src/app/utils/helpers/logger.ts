424b4bd82eb3df9dc0c1ca0a4faf9727607645de
Nadun Siriwardhana <naduns@99x.io>
2023-01-13 19:01:53 +0530

    fix(logger): change client project from react to angular

--- apps/api/src/app/utils/helpers/logger.ts
+++ apps/api/src/app/utils/helpers/logger.ts
@@ -1,3 +1,35 @@
 export class Logger {
+  private _scope: string;
+  private _logLevel: number;
 
+  constructor(scope: string, logLevel: 1 | 2 | 3) {
+    this._scope = scope;
+    this._logLevel = logLevel;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public debug(message: string, ...data: any){
+    if(this._logLevel > 0){
+      this.log('debug', message, data);
+    }
+  }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public info(message: string, ...data: any){
+      if(this._logLevel > 1) {
+        this.log('info', message, data);
+      }
+    }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public error(message: string, ...data: any){
+    if(this._logLevel > 2) {
+      this.log('error', message, data);
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private log(type: string, message: string, ...data: any){
+    console.log(`[${type}:${this._scope}]: ${message} ::: data >>> ${JSON.stringify(data)}`);
+  }
 }
