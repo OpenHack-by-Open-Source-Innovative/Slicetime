902fd181156119dcd16cabb74cceb0de905a0b95
naduni99x <102135074+naduni99x@users.noreply.github.com>
2023-01-14 03:38:26 +0530

    Auth component implementation

--- apps/client/src/app/core/components/auth/auth.component.ts
+++ apps/client/src/app/core/components/auth/auth.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl} from '@angular/forms';
+import {FloatLabelType} from '@angular/material/form-field';
 
 @Component({
   selector: 'slicetime-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {}
+export class AuthComponent {
+  hide = true;
+}
